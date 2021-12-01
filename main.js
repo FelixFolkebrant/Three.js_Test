d4eeff70b03204209d8798d4a3d80c87db9926a1
QXPP <Felix.folkebrant@gmail.com>
2021-12-01 21:06:53 +0100

    Added Files
    Added gitignore for Node as well as adding all the basic files

---
+++ main.js
@@ -0,0 +1,15 @@
+import * as THREE from "three"
+
+let FOV = 75
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(FOV, window.innerWidth/window.innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGL1Renderer({
+    canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio( window,devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render( scene, camera);
