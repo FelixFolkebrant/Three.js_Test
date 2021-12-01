25e9e191a38c47b4d42eb5fa5654e70a43b40fdf
QXPP <Felix.folkebrant@gmail.com>
2021-12-01 22:21:33 +0100

    Basic Cube
    Added a basic cube and some lightning as well as panning controll for mouse. Tried a lot of different integrations of the  three.js package but didn't manage to get any to work apart from the GitHub clone. Ignored the library for size, check documentation to re-clone it. If other option is to be pursued, go with cdn, not node package as it is not recommended by three.js

---
+++ Documentation.md
@@ -0,0 +1,9 @@
+# Documentation for three.js github library
+## Setup
+1. run "git clone --depth=1 https://github.com/mrdoob/three.js.git" in terminal to clone full project
+2. Create website as usually
+3. Import the imports in a js
+4. Done
+## Common import paths
+1. **Standard module** *'./three.js/build/three.module.js'*
+2. **OrbitControls** *'./three.js/examples/jsm/controls/OrbitalControls.js'*
--- main.js
+++ main.js
@@ -1,15 +1,47 @@
-import * as THREE from "three"
+import * as THREE from './three.js/build/three.module.js'; //Checka examples
 
-let FOV = 75
+import { OrbitControls} from './three.js/examples/jsm/controls/OrbitControls.js'
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(FOV, window.innerWidth/window.innerHeight, 0.1, 1000)
-const renderer = new THREE.WebGL1Renderer({
-    canvas: document.querySelector("#bg"),
-});
-
-renderer.setPixelRatio( window,devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight);
+const scene         = new THREE.Scene();
+const camera        = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer      = new THREE.WebGLRenderer({
+                        canvas: document.querySelector('#bg'),
+                        });
+                        
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
+camera.position.setX(-3);
 
 renderer.render( scene, camera);
+
+const geometry      = new THREE.BoxGeometry(3, 3, 3)
+const material      = new THREE.MeshStandardMaterial( { color: 0xffffff } );
+const cube         = new THREE.Mesh( geometry, material)
+
+scene.add(cube)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5,5,5)
+scene.add(pointLight)
+
+const pointLight2 = new THREE.PointLight(0xffffff)
+pointLight2.position.set(-2,-2,-2)
+scene.add(pointLight2)
+
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    // cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    controls.update();
+
+    renderer.render( scene, camera);
+}
+
+animate()
