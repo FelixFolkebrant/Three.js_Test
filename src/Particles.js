ba8578736115694b1ebd2975a65279de10b9688e
QXPP <Felix.folkebrant@gmail.com>
2021-12-02 22:45:25 +0100

    Cleaned up
    Got every site related thing in a src folder to easily navigate. Noticed that I have designed the CSS with 175% zoom but need to redesign anyway since I need to design truly responsive.

--- src/Particles.js
+++ src/Particles.js
@@ -1,32 +1,19 @@
 // Imports
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-import * as THREE from './three.js/build/three.module.js'; //Checka examples
-import { OrbitControls} from './three.js/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader} from './three.js/examples/jsm/loaders/GLTFLoader.js'
+import * as THREE from '../three.js/build/three.module.js'; //Checka examples
 
-<<<<<<< Updated upstream
-=======
+
+const bgColor = 0x272E38
 // Scene start
 const renderer      = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
 const scene         = new THREE.Scene();
+const clock         = new THREE.Clock()
 
-<<<<<<< HEAD
 // Create placeholder values with let to make variables global to use inside and outside resize listener function
-let camera, screenWidth, screenHeight, controls = 0
->>>>>>> Stashed changes
-=======
-const renderer      = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg'),
-});
-const scene         = new THREE.Scene();
->>>>>>> f1ad9e3ba7612145b4ee382f17c0f9ca7cbf2532
+let camera,mouseY,mouseX, screenWidth, screenHeight, controls;
 
-let camera = 0
-let screenWidth = 0
-let screenHeight = 0
-let controls = 0
 // Resize listener
 //---------------------------------------------------------------------------------------------------
 // Everything that should resize depending on screen should be in here
@@ -36,49 +23,47 @@ function displayWindowSize(){
     // Renderer and renderer dependencies
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(screenWidth, screenHeight);
+    renderer.setClearColor( bgColor, 1);
 
     // Camera and camera dependencies
-    camera        = new THREE.PerspectiveCamera(75, screenWidth / screenHeight, 0.1, 1000);
+    camera        = new THREE.PerspectiveCamera(75, screenWidth / screenHeight, 0.1, 10000);
     camera.position.setZ(30);
     camera.position.setX(-3);
-    controls = new OrbitControls(camera, renderer.domElement);
 }
-<<<<<<< Updated upstream
 window.addEventListener("resize", displayWindowSize);
-displayWindowSize()
-
-// Scene
-//---------------------------------------------------------------------------------------------------
-<<<<<<< HEAD
-const scene         = new THREE.Scene();
-const camera        = new THREE.PerspectiveCamera(75, screenWidth / screenHeight, 0.1, 1000);
-const renderer      = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg'),
-});
-
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(screenWidth, screenHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
-=======
-document.addEventListener("resize", displayWindowSize);
 displayWindowSize();
->>>>>>> Stashed changes
-=======
->>>>>>> f1ad9e3ba7612145b4ee382f17c0f9ca7cbf2532
 
+document.addEventListener('mousemove', animateParticles)
+function animateParticles(event){
+    mouseY = event.clientY
+    mouseX = event.clientX
+}
 // Objects
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-const geometry      = new THREE.BoxGeometry(3, 3, 3)
-const material      = new THREE.MeshStandardMaterial( { color: 0xffffff } );
-const cube          = new THREE.Mesh( geometry, material)
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
+const geometry      = new THREE.BoxGeometry(15, 15, 15, 15, 15, 15)
+const cMaterial      = new THREE.PointsMaterial( { 
+    size:0.05,
+    color: 'white',
+});
+const cube          = new THREE.Points( geometry, cMaterial)
 scene.add(cube)
 
 
+const pMaterial     = new THREE.PointsMaterial({
+    size:0.05 
+});
+const pGeometry     = new THREE.BufferGeometry;
+const pCount        = 500;
+const pArray        = new Float32Array(pCount * 3);
+
+for(let i = 0; i < pCount * 3; i++){
+
+    pArray[i] = (Math.random() -0.5)*50
+}
+
+pGeometry.setAttribute('position', new THREE.BufferAttribute(pArray, 3))
+const pMesh = new THREE.Points(pGeometry, pMaterial)
+scene.add(pMesh)
 
 
 // Lightsources
@@ -86,33 +71,28 @@ scene.add(cube)
 
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(5,5,5)
+
 scene.add(pointLight)
 
-const pointLight2 = new THREE.PointLight(0xffffff)
-pointLight2.position.set(-2,-2,-2)
-scene.add(pointLight2)
-
-<<<<<<< Updated upstream
-// Controls
-//---------------------------------------------------------------------------------------------------
-=======
 // Scroll animate
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
->>>>>>> Stashed changes
-
 
 
 // Animation
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function animate() {
     requestAnimationFrame( animate );
-    
-    // cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    
-    controls.update();
+    const elapsedTime = clock.getElapsedTime()
+    pMesh.rotation.y = 0.1 * elapsedTime
+    // Mouse panning controls 2/2
+    if(mouseX > 0){
+    pMesh.rotation.y = mouseX * (elapsedTime * 0.0002)
+    pMesh.rotation.x = mouseY * (elapsedTime * 0.00001)
+    cube.rotation.x = mouseX * (elapsedTime * 0.00002)
+    cube.rotation.y = mouseY * (elapsedTime * 0.0002)
+    }
     
     renderer.render( scene, camera);
 }
-
 animate()
+
--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 document.addEventListener('scroll', function() {
     var currScrollPos2 = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
     if (currScrollPos2 > 300) {
@@ -7,15 +11,15 @@ document.addEventListener('scroll', function() {
       document.getElementById('introMid').style.left = "-15%";
       document.getElementById('introLarge').style.left = "-10%";
       document.getElementById('introLarge').style.opacity = "0%";
-      document.getElementById('introSmall').style.left = "-15%";
-      document.getElementById('profileHolder').style.left = "-31%";
+      // sleep(2000);
+      // document.getElementById('background').style.left = "-200%";
     }
     else{
         document.getElementById('introMid').style.left = "15%";
         document.getElementById('introLarge').style.left = "10%";
         document.getElementById('introLarge').style.opacity = "100%";
         document.getElementById('introSmall').style.left = "15%";
-        document.getElementById('profileHolder').style.left = "31%";
+        document.getElementById('background').style.display = "inline";
     }
   }
 );
